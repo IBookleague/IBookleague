ebb127e61df4af2366b0839af7b7140e23b02916
Hyeong Jun Noh <munhwas1140@naver.com>
2023-11-10 00:58:34 +0900

    [refactor] User UserRole 제거

--- src/main/java/com/ibookleague/user/domain/User.java
+++ src/main/java/com/ibookleague/user/domain/User.java
@@ -18,19 +18,5 @@ public class User {
     private String username;
 
     private String password;
-
-    public enum UserRole {
-        ADMIN("ROLE_ADMIN"),
-        USER("ROLE_USER");
-
-        UserRole(String value) {
-            this.value = value;
-        }
-        private String value;
-
-        public String getValue() {
-            return value;
-        }
-    }
 }
 
